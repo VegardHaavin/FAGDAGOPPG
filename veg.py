72de80ed2e8896a5dd3f5f046092485ec90d4938
Vegardhaa <Vegzibro@outlook.com>
2024-02-20 14:05:17 +0100

    Vegards Endringer
    la inn veg.py og endret csv filen

--- veg.py
+++ veg.py
@@ -1,1 +1,14 @@
+import csv
 
+filnavn = "ver.csv"
+dag=[]
+for i in range(1,366):
+  dag.append(i)
+vind=[]
+temp=[]
+with open(filnavn, encoding="utf-8-sig") as fil:
+  filinnhold = csv.reader(fil, delimiter=";")
+  overskrifter = next(filinnhold)
+  for rad in filinnhold:
+    vind.append(float(rad[4]))
+    temp.append(float(rad[3]))
