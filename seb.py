2314feab5d2f86e3abcd2c0c36ccf18dd776aafd
sebmoen <69125023+Sebmoen1@users.noreply.github.com>
2024-02-20 14:12:23 +0100

    temperatur skrevet
    hell yeah

--- seb.py
+++ seb.py
@@ -1,1 +1,12 @@
 
+import matplotlib.pyplot as plt
+
+plt.style.use("ggplot")
+plt.figure(figsize=(10,5))
+plt.plot(dag, temp, zorder=2)
+plt.axhline(y=0, color="black", zorder=1)
+plt.title("Gjennomsnittstemperatur vær dag i 2023")
+plt.xlabel("Dag")
+plt.ylabel("Temperatur")
+plt.grid()
+plt.show()
