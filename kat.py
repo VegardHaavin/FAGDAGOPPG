654398159eb12aa873ac31b213e10a61b1dea870
katnil <katnil10@vgs.nfk.no>
2024-02-20 14:09:30 +0100

    Laget plot for vind

--- kat.py
+++ kat.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
 
+plt.style.use("ggplot")
+plt.figure(figsize=(10,5))
+plt.plot(dag, vind, zorder=2)
+plt.axhline(y=0, color="black", zorder=1)
+plt.title("Gjennomsnittsvind vær dag i 2023")
+plt.xlabel("Dag")
+plt.ylabel("Vindstyrke")
+plt.grid()
+plt.show()
